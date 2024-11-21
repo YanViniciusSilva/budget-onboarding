c870b979554afbb9ef085449137b77ecd1fe0006
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2024-11-21 08:38:34 -0300

    feat :sparkles: add environment

---
+++ environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCfZP0BxwmTphqu8EyuaxGBCtfmp13ayRE',
+    authDomain: 'albuquerque-tattoo.firebaseapp.com',
+    projectId: 'albuquerque-tattoo',
+    storageBucket: 'albuquerque-tattoo.firebasestorage.app',
+    messagingSenderId: '541620912191',
+    appId: '1:541620912191:web:fff47a8ba52a846d7c388a',
+    measurementId: 'G-RL1CB6W099',
+  },
+};
